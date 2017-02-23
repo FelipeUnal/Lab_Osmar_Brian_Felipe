beaf5bd7d61224c68a3e4945dbbf66a405da8772
Estudiante <Estudiante@10.203.2.17>
2017-02-23 16:02:08 -0500

    caso ej

---
+++ src/Futbol/Main.java
@@ -0,0 +1,13 @@
+
+package Futbol;
+
+public class Main {
+    public static void main(String[] args){
+        Futbol futbol = new Futbol();
+        
+        futbol.advanceRigth();
+        futbol.advance();
+        futbol.advanceLeft();
+    }
+    
+}
